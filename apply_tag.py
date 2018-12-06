044eb56a5d69c66a40aa064d38ea752db5bf4616
jnetz <cwgraven@gmail.com>
2018-12-06 14:00:01 -0500

    Second iteration of apply_tag.py

--- apply_tag.py
+++ apply_tag.py
@@ -1,3 +1,35 @@
+"""
+Description: apply_tag.py is used to update a list of systems with a specified ePO tag. ePO has some limitations as
+to the number of discreet "or" rules placed inside of tagging logic. As such, if you need to create a list longer
+than about 50 systems, you may have to perform the exercise in batches. This program will take an entire list of
+systems as long as you like and update all of them with the tag selected.
+
+usage: apply_tag.py [-h] --host HOST [--port PORT] --un UN --pw PW --file FILE
+                    --tag TAG [-v]
+
+Update systems in ePO with given tag
+
+optional arguments:
+  -h, --help   show this help message and exit
+  --host HOST  ePO FQDN or IP Address
+  --port PORT  ePO port
+  --un UN      ePO username with authorization to access API. This must be a
+               superuser
+  --pw PW      ePO password
+  --file FILE  The file containing the system names to be updated
+  --tag TAG    The new tag you'd like to add to the systems. This tag must
+               already exist in the tag catalog
+  -v           Verbose output. This will notify you of every system which is
+               being updated.
+
+"""
+__author__ = "Jesse Netz"
+__copyright__ = "Copyright 2018, The Open McAfee Project"
+__license__ = "GPL"
+__version__ = "1.0"
+__maintainer__ = "Jesse Netz"
+__email__ = "jesse.netz@icloud.com"
+
 import mcafee
 import argparse
 
